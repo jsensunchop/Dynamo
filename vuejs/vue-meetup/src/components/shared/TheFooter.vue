d9d109eef9eddb426cbb0e3defa2e0fdf06d8710
jsensunchop <jsensunchop@unal.edu.co>
2019-11-11 20:06:28 -0500

    :lipstick: Updating the UI
    New front added

---
+++ vuejs/vue-meetup/src/components/shared/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Dynamo-Store</strong> by <a href="https://jgthms.com">Dynamo Team</a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
