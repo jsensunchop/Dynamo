e7c2cabc76823746e7948a4474a81e610f53afbf
jsensunchop <jsensunchop@unal.edu.co>
2019-12-08 17:49:55 -0500

    :consruction: interceptors added

--- vuejs/vue-meetup/src/services/axios.js
+++ vuejs/vue-meetup/src/services/axios.js
@@ -3,7 +3,19 @@ import axios from 'axios'
 const axiosInstance = axios.create({
     //request timeout
     timeout: 3000,
-    headers: {'authorization': `Bearer ${(localStorage.getItem('meetuper-jwt') || '')}`}
+    //removing headers
+    //headers: {'authorization': `Bearer ${(localStorage.getItem('meetuper-jwt') || '')}`}
+})
+
+axiosInstance.interceptors.request.use(function(config) {
+    const token =  localStorage.getItem('meetuper-jwt') || ''
+
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+}, function (err) {
+    return Promise.reject(err)
 })
 
 export default axiosInstance
