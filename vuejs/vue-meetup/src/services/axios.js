3d1cfd6fe71f91fecd80ecee0349207d4d087b61
jsensunchop <jsensunchop@unal.edu.co>
2019-12-08 16:56:32 -0500

    :lock: problem with axios and jwt

--- vuejs/vue-meetup/src/services/axios.js
+++ vuejs/vue-meetup/src/services/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
     //request timeout
     timeout: 3000,
-    headers: {'authorization': `Bearer ${(localStorage.getItem('vue-meetup') || '')}`}
+    headers: {'authorization': `Bearer ${(localStorage.getItem('meetuper-jwt') || '')}`}
 })
 
 export default axiosInstance
