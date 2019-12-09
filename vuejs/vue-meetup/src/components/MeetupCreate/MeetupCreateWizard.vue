feebac5a44dd95811c68b6e2dbdf7d158c1824d0
jsensunchop <jsensunchop@unal.edu.co>
2019-12-09 00:05:56 -0500

    :sparkles: Adding new features

---
+++ vuejs/vue-meetup/src/components/MeetupCreate/MeetupCreateWizard.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="meetup-create-form">
+        <div class="current-step is-pulled-right">
+            1 of 4
+        </div>
+        <MeetupLocation />
+        <MeetupDetail />
+        <MeetupDescription />
+        <MeetupConfirmation />
+
+        <progress class="progress" :value="100" max="100"> 100%</progress>
+        <div class="controll-btns m-b-md">
+            <button class="button is-primary m-r-sm">Atras</button>
+            <button class="button is-primary">Siguiente</button>
+        </div>
+        <pre><code>{{form}}</code></pre>
+    </div>
+</template>
+
+<script>
+import MeetupLocation from './MeetupLocation'
+import MeetupDetail from './MeetupDetail'
+import MeetupDescription from './MeetupDescription'
+import MeetupConfirmation from './MeetupConfirmation'
+export default {
+    components: {
+    MeetupLocation,
+    MeetupDetail,
+    MeetupDescription,
+    MeetupConfirmation
+    },
+    data(){
+        return{
+            form: {
+                location: null,
+                title: null,
+                startDate: null,
+                category: null,
+                image: null,
+                shortInfo: null,
+                description: null,
+                timeTo: null,
+                timeFrom: null
+            }
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+    .meetup-create-form{
+        box-sizing: border-box;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 840px;
+        padding: 24px 16px 8px;
+        width: 100%;
+    }
+</style>
---
+++ vuejs/vue-meetup/src/components/MeetupCreate/MeetupLocation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 class="title m-b-sm">Cual es la ubicación de tu Meetup?</h1>
+    <div class="m-b-lg">
+      <span class="subtitle">Bogotá, COL</span>
+      <a>(cambiar ubicación)</a>
+      <input v-model="form.location" type="text" class="input">
+      <div v-if="$v.form.location.$error">
+        <span v-if="!$v.form.location.required" class="help is-danger">La ubicación es requerida</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { required } from 'vuelidate/lib/validators'
+  export default {
+    data () {
+      return {
+         form: {
+           location: null
+        }
+      }
+    },
+    validations: {
+      form: {
+        location: required
+      }
+    }
+  }
+</script>
+
+<style scoped>
