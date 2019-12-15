c3df1caea5eaeb424b9e2cf5ed90cdf701ca2849
jsensunchop <jsensunchop@unal.edu.co>
2019-12-15 14:06:31 -0500

    :boom: peticion a la api para creacion de reuniones

---
+++ vuejs/vue-meetup/server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports={
+    
+    DB_URI: 'mongodb+srv://TestUser:TestUser@cluster0-c4zss.mongodb.net/test?retryWrites=true&w=majority',
+    SESSION_SECRET: 'FR1QUHLSso',
+    JWT_SECRET: 'Sgz1Qv5-oH'
+};
+
