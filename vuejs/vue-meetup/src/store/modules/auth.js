2b385ed5d8aa7e7a215d200b696e7c37406cfefb
jsensunchop <jsensunchop@unal.edu.co>
2019-12-17 17:38:59 -0500

    :sparkles: profile added + functionalities

--- vuejs/vue-meetup/src/store/modules/auth.js
+++ vuejs/vue-meetup/src/store/modules/auth.js
@@ -105,8 +105,15 @@ export default {
 
       userMeetupsIds.splice(index, 1)
       commit('setMeetupsToAuthUser', userMeetupsIds)
+    },
+    updateUser ({commit}, user) {
+      return axiosInstance.patch(`/api/v1/users/${user._id}`, user)
+        .then(res => {
+          const updatedUser = res.data
+          commit('setAuthUser', updatedUser)
+          return updatedUser
+        })
     }
-
   },
   mutations: {
     setAuthUser (state, user) {
