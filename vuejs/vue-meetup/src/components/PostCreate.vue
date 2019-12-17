32d5d00feb1455e985ba1a85d9eb3b0ba45aaaa0
jsensunchop <jsensunchop@unal.edu.co>
2019-12-17 14:00:41 -0500

    :lipstick: new features

---
+++ vuejs/vue-meetup/src/components/PostCreate.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="post-create">
+    <div class="field">
+      <textarea v-auto-expand
+                class="textarea textarea-post"
+                placeholder="Escribe un comentario"
+                rows="1"></textarea>
+      <button :disabled="true" class="button is-primary m-t-sm">Enviar</button>
+    </div>
+  </form>
+</template>
+
+<script>
+  import withWarning from '@/directives/withWarning'
+  import autoExpand from '@/directives/autoExpand'
+  export default {
+      directives: {autoExpand}
+  }
+</script>
+
+<style scoped lang="scss">
+  .textarea-post {
+    padding-bottom: 30px;
+  }
+  .post-create {
+    margin-bottom: 15px;
+  }
+</style>
